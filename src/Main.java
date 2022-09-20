58d5833750bf2c5081affc85ce2c69d47acd01ac
tKons64 <spb_unex@mail.ru>
2022-09-21 00:03:41 +0300

    Выполнены задача повышенной сложности №9

--- src/Main.java
+++ src/Main.java
@@ -186,7 +186,7 @@ public class Main {
         int total = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 if (i == j) {
                     continue;
                 }
